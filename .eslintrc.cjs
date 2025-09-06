f82bae493809a8926a54d129fff93fe4ec6b7cdd
brianvoe <brian@webiswhatido.com>
2025-09-06 10:53:55 -0500

    lint - added eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,23 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-  ],
+  extends: ['eslint:recommended', 'prettier'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
     'prefer-const': 'error',
     'no-var': 'error',
+    'prettier/prettier': 'error',
+    'import/newline-after-import': 'error',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'never',
+      },
+    ],
   },
-}
+};
