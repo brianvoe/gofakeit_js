8be914c96d088dba902ca2a2a2e08946558e412a
brianvoe <brian@webiswhatido.com>
2025-10-15 13:47:50 -0500

    vite - docs config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath } from 'url'
 import { resolve, dirname } from 'path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 import vue from '@vitejs/plugin-vue'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -19,11 +18,5 @@ export default defineConfig({
     sourcemap: true,
     minify: true,
   },
-  plugins: [
-    vue(),
-    dts({
-      insertTypesEntry: true,
-      rollupTypes: true,
-    }),
-  ],
+  plugins: [vue()],
 })
